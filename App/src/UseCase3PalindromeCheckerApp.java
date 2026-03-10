e34f040d4bac0d1894e30ea4f8062a18fa420207
lucky130707 <harshateja1307.p@gmail.com>
2026-03-10 10:26:38 +0530

    UC 3

---
+++ App/src/UseCase3PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class UseCase3PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        String reversed = "";
+
+        // Reverse string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        // Compare original and reversed string
+        if (original.equals(reversed)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
